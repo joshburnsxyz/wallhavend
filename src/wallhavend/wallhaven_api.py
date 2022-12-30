61cf58f059d31770bc59ff6626eb1b2178822460
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-12-30 02:04:46 +0000

    dont need requests imported into payload generator

--- src/wallhavend/wallhaven_api.py
+++ src/wallhavend/wallhaven_api.py
@@ -1,6 +1,13 @@
-import requests
-
 def make_payload(query,nsfw_flag,target_page):
+  """
+  Generates a valid data payload for making requests to
+  the wallhaven API.
+
+  Params: 
+    - query - The search term used.
+    - nsfw_flag - 3 digit binary mask for filtering images.
+    - target_page - Results from the API are paged, we can target pages individually.
+  """
   p = {
     "q": query,
     "purity": nsfw_flag,
