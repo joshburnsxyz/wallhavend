934e9c1836816a6312bb8d9c37e4cdf100bfc3e1
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-12-30 00:31:14 +0000

    nsfw filter flag

--- src/wallhavend/console.py
+++ src/wallhavend/console.py
@@ -1,5 +1,5 @@
 import argparse
-import os
+import requests
 
 # Create parser 
 parser = argparse.ArgumentParser(
@@ -9,11 +9,27 @@ parser = argparse.ArgumentParser(
 
 # Flags
 parser.add_argument('-k', '--key', metavar="KEY", help="Your wallhaven.cc API key")
+parser.add_argument('-q', '--query', metavar="QUERY", help="Search term, or tag")
+parser.add_argument('-N', '--nsfw', action="store_true", help="Allow NSFW images to be returned in results")
 
 def run():
   args = parser.parse_args()
+  base_url = "https://wallhaven.cc/api/v1/search"
+  nsfw_flag = "100"
 
-  # Print request URL
-  print(f"https://wallhaven.cc/api/v1/search?apikey={args.key}")
-  
-  
+  # Append API Key to request URL if available
+  if args.key is not None:
+    base_url += f"?apikey={args.key}"
+
+  # NSFW filter
+  if args.nsfw is True:
+    nsfw_flag = "111"
+
+  # Generate payload for request
+  payload = {
+    "q": args.query,
+    "purity": nsfw_flag
+  }
+
+  wallhaven_search_request = requests.get(base_url, params=payload)
+  data = wallhaven_search_request.json()
